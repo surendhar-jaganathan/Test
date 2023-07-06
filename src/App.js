b08497f3ec4a738fe54ed4dd7f1a1ad4790075fe
j.surendhar11@gmail.com <j.surendhar11@gmail.com>
2023-07-06 13:22:33 +0530

    get token

--- src/App.js
+++ src/App.js
@@ -5,9 +5,33 @@ import { BrowserRouter } from 'react-router-dom';
 
 
 function App() {
+
+  const getdata=()=>{
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Authorization", "Basic d2Vrbm93bG9uZG9uQHByaW9hcGlzLmNvbTpGIzZ3cE1VSEBXUlJXZ1A=");
+    
+    var raw = JSON.stringify({
+      "grant_type": "client_credentials"
+    });
+    
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+    
+    fetch("https://sandbox-distributor-api.prioticket.com/v3.5/distributor/oauth2/token", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+  }
   return (
+    <div>
  <h1>i am surendhar</h1>
-  
+ <h2 onClick={getdata}>get token </h2>
+ </div>
      
   );
 }
