8bf68d60b01089ac5bef556d86a5d187a2afc24f
j.surendhar11@gmail.com <j.surendhar11@gmail.com>
2023-07-06 20:23:50 +0530

    removebasic

--- src/App.js
+++ src/App.js
@@ -7,27 +7,7 @@ import { useHistory } from 'react-router-dom';
 
 function App() {
  const history = useHistory();
-  const getdata=()=>{
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    myHeaders.append("Authorization", "Basic d2Vrbm93bG9uZG9uQHByaW9hcGlzLmNvbTpGIzZ3cE1VSEBXUlJXZ1A=");
-    
-    var raw = JSON.stringify({
-      "grant_type": "client_credentials"
-    });
-    
-    var requestOptions = {
-      method: 'POST',
-      headers: myHeaders,
-      body: raw,
-      redirect: 'follow'
-    };
-    
-    fetch("https://sandbox-distributor-api.prioticket.com/v3.5/distributor/oauth2/token", requestOptions)
-      .then(response => response.text())
-      .then(result => console.log(result))
-      .catch(error => console.log('error', error));
-  }
+  
   const getdatatwo=()=>{
     
 history.push("./about")
